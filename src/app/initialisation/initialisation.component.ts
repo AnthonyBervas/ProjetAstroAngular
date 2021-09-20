bbe46b9486abd358886fc8b473910fcb0a0e2495
Yannis <yannis.benrida@gadz.org>
2021-09-20 10:29:47 +0200

    component

---
+++ src/app/initialisation/initialisation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-initialisation',
+  templateUrl: './initialisation.component.html',
+  styleUrls: ['./initialisation.component.css']
+})
+export class InitialisationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
