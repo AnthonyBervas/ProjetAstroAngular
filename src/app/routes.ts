53d0b78b7fbb6ee00c883486bf9ef28b3228363d
Abervas <anthony.bervas1@gmail.com>
2021-09-20 16:17:53 +0200

    Auto stash before merge of "master" and "origin/master"
    Update Security

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,24 +3,30 @@ import { InscriptionComponent } from './inscription/inscription.component';
 import { MenuComponent } from './menu/menu.component';
 import { LoginComponent } from './login/login.component';
 import { CreateComponent } from './create/create.component';
+import {UserCanActivateService} from "./services/user-can-activate.service";
+import {NoAuthCanActivateService} from "./services/no-auth-can-activate.service";
 import { ModificationComponent } from './modification/modification.component';
 
 export const routes: Routes = [
   {
     path: 'inscription',
     component: InscriptionComponent,
+    canActivate: [NoAuthCanActivateService],
   },
   {
     path: 'menu',
     component: MenuComponent,
+    canActivate: [UserCanActivateService],
   },
   {
     path: 'compte',
     component: LoginComponent,
+    canActivate: [NoAuthCanActivateService],
   },
   {
     path: 'create',
     component: CreateComponent,
+    canActivate: [UserCanActivateService],
   },
   {
     path: 'Modification',
