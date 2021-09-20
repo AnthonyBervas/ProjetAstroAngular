0396031b93a05a2b0d20e9efd2ccd1d445d2d217
Raistlin <victor.douet38@gmail.com>
2021-09-20 16:49:09 +0200

    Revert "modif route"
    This reverts commit 2d81f663caa2c0aac0cd926bc8d1b8dab3fd32b0.

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,13 +4,8 @@ import { InscriptionComponent } from './inscription/inscription.component';
 import { MenuComponent } from './menu/menu.component';
 import { LoginComponent } from './login/login.component';
 import { CreateComponent } from './create/create.component';
-<<<<<<< HEAD
-import { UserCanActivateService } from './services/user-can-activate.service';
-import { NoAuthCanActivateService } from './services/no-auth-can-activate.service';
-=======
 import {UserCanActivateService} from "./services/user-can-activate.service";
 import {NoAuthCanActivateService} from "./services/no-auth-can-activate.service";
->>>>>>> master
 
 export const routes: Routes = [
   {
