2d81f663caa2c0aac0cd926bc8d1b8dab3fd32b0
Yannis <yannis.benrida@gadz.org>
2021-09-20 16:42:02 +0200

    modif route

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,9 +4,8 @@ import { InscriptionComponent } from './inscription/inscription.component';
 import { MenuComponent } from './menu/menu.component';
 import { LoginComponent } from './login/login.component';
 import { CreateComponent } from './create/create.component';
-import {UserCanActivateService} from "./services/user-can-activate.service";
-import {NoAuthCanActivateService} from "./services/no-auth-can-activate.service";
-import { ModificationComponent } from './modification/modification.component';
+import { UserCanActivateService } from './services/user-can-activate.service';
+import { NoAuthCanActivateService } from './services/no-auth-can-activate.service';
 
 export const routes: Routes = [
   {
